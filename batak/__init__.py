ec4e507d96d5c62138ae8f710a24e1ed022c8fef
Görkem <buzcu@users.noreply.github.com>
2025-12-19 09:34:59 +0100

    Game rules refactor (#8)
    * fixed a rule bug that let player to underplay a trump card, and added auto simulation mode
    
    * fixed formatting and too long functions

--- batak/__init__.py
+++ batak/__init__.py
@@ -4,7 +4,7 @@ from .deck import Deck as Deck
 from .player import Player as Player
 from .player import BotPlayer as BotPlayer
 from .player import HumanPlayer as HumanPlayer
-from .game import Game as Game
+from .game import Batak as Batak
 from .play_strategies import PlayStrategies as PlayStrategies
 from .play_strategies import RandomPlayStrategies as RandomPlayStrategies
 from .play_strategies import HighCardPlayStrategies as HighCardPlayStrategies
